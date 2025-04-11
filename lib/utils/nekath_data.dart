d468dd2c19dc2d4579a6c71c1203bfcbe3b38922
Methexx <Methum@1234>
2025-04-12 00:25:15 +0530

    add more nekath

--- lib/utils/nekath_data.dart
+++ lib/utils/nekath_data.dart
@@ -1,7 +1,12 @@
 import '../models/nekath.dart';
 
 List<Nekath> sampleNekathList = [
-  Nekath(title: "නව සද බැලීම", dateTime: DateTime(2025, 4, 14, 7, 42)),
-  Nekath(title: "පරණ අවුරුද්ද සදහා ස්නානය", dateTime: DateTime(2025, 4, 14, 10, 12)),
-  Nekath(title: "අළුත් අවුරුදු උදාව", dateTime: DateTime(2025, 4, 14, 12, 34)),
+  Nekath(title: "නව සද බැලීම", dateTime: DateTime(2025, 3, 30, 12, 00)),
+  Nekath(title: "පරණ අවුරුද්ද සදහා ස්නානය", dateTime: DateTime(2025, 4, 13, 10, 12)),
+  Nekath(title: "අළුත් අවුරුදු උදාව", dateTime: DateTime(2025, 4, 14, 03, 21)),
+  Nekath(title: "පුණ්‍ය කාලය", dateTime: DateTime(2025, 4, 14, 08, 57)),
+  Nekath(title: "අහාර පිසීම", dateTime: DateTime(2025, 4, 14, 04, 04)),
+  Nekath(title: "වැඩ ඇල්ලීම, ගණුදෙනු කිරීම හා ආහාර අනුබවය", dateTime: DateTime(2025, 4, 14, 06, 44)),
+  Nekath(title: "හිත තෙල් ගෑම", dateTime: DateTime(2025, 4, 16, 09, 04)),
+  Nekath(title: "රැකීරක්ශා සදහා පිටත්ව යාම", dateTime: DateTime(2025, 4, 17, 09, 03)),
 ];
